8aabfcb47ff64a1635b30f7ebd1dcd2f29868833
Mark Mcloughlin <mark25mky@gmail.com>
2024-05-20 15:06:15 +0000

    added board class

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+class Ship:
+    def __init__ (self, size, orientation, start):
+        self.size = size
+        self.orientation = orientation
+        self.start = start
+        self.coordinates = self.generate_coordinates()
+
+    def generate_coordinates(self):
+        coordinates = []
+        x, y = self.start
+        for i in range(self.size):
+            if self.orientation == 'H': 
+                coordinates.append((x, y + i))
+            else: # 'V'
+                    coordinates.append((x + i, y))
+    return coordinates
+
+    class Board:
+        def __init__ (self, size):
+            self.size = size
+            self.grid = [['~' for _ in range(size)] for _ in range(size)]
+            self.ships = []
+
+        def place_ship(self, ship):
+            for (x, y) in ship.coordinates:
+                self.grid[x][y] = 'S'
+        self.ships.append(ship)
+        return True
+        
